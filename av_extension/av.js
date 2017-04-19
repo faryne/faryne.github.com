15ca17cfdfe24018f516cf25ac1907979800918f
Faryne <farynehsieh@omusic.com.tw>
2017-04-19 12:31:33 +0800

    AV圖 extension - 最終修改版本

--- av_extension/av.js
+++ av_extension/av.js
@@ -1,8 +1,31 @@
 var imgs = document.getElementsByTagName('img');
-
-var output = [];
-for (var i in imgs)
+function get_imgs ()
 {
-    output.push(imgs[i].src);
+    var response = JSON.parse(request.responseText);
+    var imgs = [];
+    for (var i in response)
+    {
+        imgs.push(response[i].thumb);
+    }
+
+    var elements = document.getElementsByTagName('img');
+    for (var i in elements)
+    {
+        if (elements[i].width > 70 && elements[i].width < 200)
+        {
+            var key = Math.floor(Math.random()*imgs.length);
+            elements[i].src = imgs[key];
+            elements[i].style.height = "";
+            elements[i].removeAttribute('height');
+        }
+    }
 }
-alert(output.join(";"));
+
+var request;
+window.onload = function()
+{
+    request = new XMLHttpRequest();
+    request.onreadystatechange = get_imgs;
+    request.open('GET', 'https://d9ymae9qjseyw.cloudfront.net/dmm/ranking_online_weekly.json');
+    request.send();
+};
